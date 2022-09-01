9f063de468bd46f03841b83d5f3b8702c83f7528
zxx <ss15037500392@163.com>
2022-09-01 21:36:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <router-view></router-view>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+html,
+body,
+#app,
+.el-container,
+.el-menu{
+  height: 100%;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>我是一个首页呀</div>
+    <el-button type="primary">Primary</el-button>
+   <ChatRound />
+</template>
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
